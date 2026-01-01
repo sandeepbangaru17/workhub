ec3f134f12bab726ca38d6feb7a4069ce204df90
Sandeep <sandeepbangaru17@gmail.com>
2026-01-01 22:24:06 +0530

    Connected sqlite db to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
 
 const app = express();
 const PORT = 5000;
+
+
+app.use(express.json());
+const db = new sqlite3.Database("./database/workhub.db", (err) => {
+  if (err) {
+    console.error("Database connection error:", err.message);
+  } else {
+    console.log("Connected to SQLite database");
+  }
+});
+
 
 // test route
 app.get("/", (req, res) => {
